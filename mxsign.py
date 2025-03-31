7d3b7334c2fee680c3e70b9a1873181ab77d330e
沉沦 <354769733@qq.com>
2025-04-01 06:59:57 +0800

    Update mxsign.py

--- mxsign.py
+++ mxsign.py
@@ -10,29 +10,29 @@ import random
 import time
 
 def mx(cookie):
-    url = "https://mox.moxing.app/forum/sign"
+    url = "https://mox.moxing.app/api/forum/check-in/sign"
     headers = {
-        'accept': 'text/html, application/xhtml+xml',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'content-type': 'application/json',
-        'dnt': '1',
-        'priority': 'u=1, i',
-        'referer': 'https://mox.moxing.app/forum/sign',
-        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0',
-        'x-inertia': 'true',
-        'x-inertia-version': 'e6f5ccfa8787fa1408a0ffa93a81c304',
-        'x-requested-with': 'XMLHttpRequest',
-        'x-xsrf-token': 'eyJpdiI6Ik1pVnhBZ1c3LzFUMTVWalpWeVIvb3c9PSIsInZhbHVlIjoiSFNXakgrNUh2M1krVkNXQ3N2ZFlQd1ZkMDZtUjhKQXg2U3dtbHRrdjZlRkZxb3VrM1dPL0I0T1VTckpqbEE5UlF1N1pCNVU1SjB6L1gyekg0VElFLzcrTXdoTEU4U3lDbktSK2h2SGJZUVBReEZsamZsbHVxU25yZXNmTXQ2QWoiLCJtYWMiOiJiZDE3YjBmYzM4ZDllMDg4Mjk1MmZjMDk1MDEyZGJmZmU5MjY2M2E2ZDJkY2E3MWQ3OGRmM2FlNTM3NGJhOGRkIiwidGFnIjoiIn0=',
-        # Requests sorts cookies= alphabetically
-        'cookie': cookie,
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    # Already added when you pass json=
+    # 'content-type': 'application/json',
+    'dnt': '1',
+    'origin': 'https://mox.moxing.app',
+    'priority': 'u=1, i',
+    'referer': 'https://mox.moxing.app/forum/sign',
+    'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-xsrf-token': 'eyJpdiI6IlFjNlcraTVJbUNnLzVzUTdTTWQ0VkE9PSIsInZhbHVlIjoiMStRMWN0clAyQ1FxQ1NFVEZ1QzhraDFQb21XQkg2RGVCRlhHa2xwYWVuS25CWTNjbXJIQmJyNHRlODlPUzQ3SWI1YTFiK3pFdFVlTWY2eGlyRkxsKzAxL0ZRQXNoblpRZXpZeENtVHdHQU5OYlRSa0RMN0VZellRdnVmUTJiaCsiLCJtYWMiOiI5YjljNzU2ZDIyNzZhMDkxYjdmN2RlNGFkYTgyOWNlNWQwYmE4OWQ3MDJjZTZlNmE4ZTgxOGJkYzZkYjExYmZhIiwidGFnIjoiIn0=',# Requests sorts cookies= alphabetically
+    'cookie': cookie,
     }
-    response = requests.get(url,headers=headers)
+    json_data = {}
+    response = requests.get(url,headers=headers, json=json_data)
     if response.status_code == 200:
         print("成功访问")
     return response.json()
